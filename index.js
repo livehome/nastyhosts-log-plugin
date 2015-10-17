c2951979650fd62c31aa46fc88711c25d76dfe68
Kacper Żuk <kacper.b.zuk@gmail.com>
2015-10-17 21:40:56 +0200

    Update for new nastyhosts

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const fs = require("fs");
 
-const log_send =
-module.exports = function(config) {
-  return {
-    middleware: function(req, res, next) {
-      const res_send = res.send.bind(res);
-      res.send = function(data) {
-        res_send.call(this, data);
-        fs.stat("log.txt", function(err, stats) {
-          if(err || stats.size < 20*1024*1024) {
-            const log = JSON.stringify({
+module.exports = function(app, config) {
+  app.use(function(req, res, next) {
+    const res_send = res.send.bind(res);
+    res.send = function(data) {
+      res_send.call(this, data);
+      fs.stat("log.txt", function(err, stats) {
+        if(err || stats.size < 20*1024*1024) {
+          const log = JSON.stringify({
               timestamp: Date.now(),
               data
-            });
-            fs.appendFile("log.txt", log+"\n");
-          }
-        });
-      };
-      next();
-    }
-  };
+          });
+          fs.appendFile("log.txt", log+"\n");
+        }
+      });
+    };
+    next();
+  });
 };
